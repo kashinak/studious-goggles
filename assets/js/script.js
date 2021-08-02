0df30c1b815499a649167857e077a3faa9fea42c
kashinak <kashinakfx@gmail.com>
2021-08-02 03:14:26 +0000

    fix game board responsiveness to fit mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,103 @@
-//declare game variables//
-/*free code camp*/
-let order = [];//variable to keep track of the order of the lights and how they are going to flash
-let playerOrder = []; //order player is pressing the lights in
-let flash; //this will be an integer - the number of flashes in the game
-let turn; //keep track of what turn we are on
-let good; /*is a boolean true/false value - whether the player is doing well or not - if the player has hit all the right colors or has not hit all the right colors*/
-let compTurn; /*boolean true/false to keep track of whether it is the computer's turn or the player's turn*/
+//game variables
+let gamePlay = [];//array to keep track of the computer's order of tile selections
+let userPlay = []; //array to keep track of player's order of tile selections
+let flash; //the number (integer) of tile flashes in the game 
+let round; //keep track of what round we are on
+let ace; /*is a boolean true/false value - whether or not 
+the player has aced hitting the correct sequence of tiles*/
+let compTurn; /*boolean true/false to keep track of 
+whether it is the computer's turn or the player's turn*/
 let intervalId; //will explain later
-let noise = true; //if we are playing noise
-let on = false; //if the program has been turned on - the power button which begins in off position so false
-let win; //This will say whether the player has won the game or not
+let sound = true; //if we are playing sound
+let run = false; //if the game program is running
+let win; //if the player has won the game or not
 
-/*my code*/
-let round = 0;
-let score = 0;
-let highScore = [];
-let num = 4;
-/*my code*/
-const startButton = document.getElementById("start");
-/*free code camp*/
-const turnCounter = document.querySelector("#turn"); //the digital read out screen for the game
-const topLeft = document.querySelector("#topleft");
-const topRight = document.querySelector("#topright");
-const bottomLeft = document.querySelector("#bottomleft");
-const bottomRight = document.querySelector("#bottomright");
-const onButton = document.querySelector("#on");
+const startButton = document.getElementById("start"); 
+const roundCount = document.getElementById("round"); 
+const topLeft = document.getElementById("topleft"); 
+const topRight = document.getElementById("topright"); 
+const bottomLeft = document.getElementById("bottomleft"); 
+const bottomRight = document.getElementById("bottomright"); 
+// const onButton = document.getElementById("on"); 
+const resetGame = document.getElementById('reset-button'); 
 
-/*my code*/
-const scoreCounter = document.querySelector("#score");
-const highScoreCounter = document.querySelector("#high-score");
+//game reset
+resetGame.addEventListener("click", reset)
 
+function reset() {
+  startGame();
+}
 
-  /*my code*/
-//upon clicking startButton, game console clears and new game sequence is played//
-
+//upon clicking startButton, game console clears and new game sequence is played
 startButton.addEventListener("click", startGame);
 
 function startGame() {
     startButton.innerHTML = "Start Game!";
     startButton.innerHTML = "";
-    // gameSequence();
     play();
 }
 
 function play() {
-    win = false; //you are starting the game you haven't won yet.
-    order = [];//we are going to use a for loop below to randomly fill up this order array
-    playerOrder = [];
+    win = false; //upon game starting, player has NOT won yet
+    gamePlay = [];//use a for loop below to randomly fill up this gamePlay array
+    userPlay = [];
     flash = 0;
     intervalId = 0;
-    turn = 1;
-    turnCounter.innerHTML = 1;//when you press start digital screen will read 1 because you are on first round of the game
-    good = true;//means the player has not hit anything incorrect yet
-    for (var i = 0; i < 20; i++) { /*fist initialize a variable i=0, we are going to loop while i is less than 20 and at the end of the loop we are going to incremnet i.*/
-        //this is how we fill the order array
-      order.push(Math.floor(Math.random() * 4) + 1);/*gets random number between 1 and 5 and it is going to be a decimal so Math.floor is going to round it down. If you console.log(order); you can see that the order array fills with 20 random numbers in console*/
+    round = 1;
+    roundCount.innerHTML = 1;
+    /*when you press startButton, the round will be set to "1"
+    because you are on first round of the game*/
+    ace = true;//means the player has not hit anything wrong
+    for (var i = 0; i < 20; i++) { 
+        //this is how we fill the gamePlay array
+      gamePlay.push(Math.floor(Math.random() * 4) + 1);
+      /*gets random number between 1 and 5 and it is going to be a 
+      decimal so Math.floor is going to round it down. 
+      The gamePlay array fills with 20 random numbers*/
     }
-    compTurn = true; //game starts with the computer flashing lights and then the player has to match those lights
+    compTurn = true; 
+    /*game starts with the computer flashing lights and then 
+    the player has to match those lights*/
       
-      //then we start the first turn
-  
-    intervalId = setInterval(gameTurn, 800);/*setInterval means that it is going to run a function after a certain amount of milliseconds. setInterval will run a gameTurn function
-      every 800 milliseconds. so a game light will flash every 800 milliseconds and keep repeating until this interval is cleared. The interval will clear after all the lights have flashed. I haven't done that yet.The reason we have an IntervalId is so we can clear the interval. If you turn the power off, clear interval because it will stop simon from running colors and will stop running the gameTurn function every 800 millliseconds. It's the gameTurn function that flashes the colors*/
+    //then we start the first turn
+    intervalId = setInterval(gameTurn, 800);
+    /*setInterval will run a gameTurn function 
+    every 800 milliseconds and will repeat until this interval is cleared 
+    and the tiles will STOP flashing when the game stops running*/
   }
   
-  /*my code*/
-//game generates a sequence with a random tile click//
-// function gameSequence() {
-//     init();
-//     let n = Math.floor(Math.random() * num + 1);
-//     setTimeout(() => {
-//         switch (n) {
-//             case 1:
-//                 playSound1();
-//                 order.push(1); //my code
-//                 break;
-//             case 2:
-//                 playSound2();
-//                 order.push(2); //my code
-//                 break;
-//             case 3:
-//                 playSound3();
-//                 order.push(3); //my code
-//                 break;
-//             case 4:
-//                 playSound4();
-//                 order.push(4);//my code
-//                 break;
-//             default:
-//         }
-//     }, 1000);
-//     // end();
-// }
 
-//end function
-// function end() {
-//     startButton.style.display = "";
-// }
+/*gameTurn function which cause the game tiles to flash yellow. */
 
-/*now let's define the gameTurn function which cause the game to flash colors. whne the colors are flashing the player is unable to click the butoons so that's wht we set on = false.*/
 function gameTurn() {
- on = false; //player cannot click any of the button swhile the game is flashing colors.
+ run = false; 
+ /*when set to false, player cannot click any of the buttons while the game is flashing colors.*/
  init() 
- if (flash == turn) { /*if the numbers of time the lights have have flashed equals the turn that we are on then that means the computer's turn is over. so if the computer's turn is over, we are going to clear interval*/
+ if (flash == round) { /*if the numbers of time the lights have have flashed 
+  equals the turn that we are on then that means the computer's 
+  turn is over. so if the computer's turn is over, we are going to clear interval*/
    clearInterval(intervalId);
    compTurn = false;
    clearColor();
-   on = true; //if on = true that means the player can now start pressing buttons.
+   run = true; //if run = true that means the player can now start pressing buttons.
  }
 
- if (compTurn) { /*the thing in the parenthesis is the condition and then you are going to run the thing in the cury brackets*/
-   clearColor(); //setTimeout is very similar to setInterval which repeats something over and over, set timeout is going to do something once after a certain number of milliseconds
-   setTimeout(() => {
-     if (order[flash] == 1) playSound1(); //if condition is true run one function and so on
-     if (order[flash] == 2) playSound2();
-     if (order[flash] == 3) playSound3();
-     if (order[flash] == 4) playSound4();
+ if (compTurn) { /*the thing in the parenthesis is the condition and then 
+  you are going to run the thing in the cury brackets*/
+   clearColor(); /*setTimeout is very similar to setInterval which repeats something over and over, 
+   set timeout is going to do something once after a certain number of milliseconds*/
+   setTimeout(function() { //my code - I converted arrow function to vanilla JS
+     if (gamePlay[flash] == 1) playSound1(); //if condition is true run playSound1() function and so on
+     if (gamePlay[flash] == 2) playSound2();
+     if (gamePlay[flash] == 3) playSound3();
+     if (gamePlay[flash] == 4) playSound4();
      flash++; //the computer goes up one flash after every 200 milliseconds
    }, 200);//I changed from 200 to 1500
  }
 }
 
-
 //link game photo tiles with corresponding game sounds and blink capability//
 function playSound1() {
-    // console.log("Hello!");
     document.getElementById("game-photo-1").style.outline =
         "#ffff00 solid 10px";
     document.getElementById("beluga-sound").play();
@@ -176,51 +149,18 @@ function blink(id) {
     // setTimeout(function() {
     //    f.style.display = (f.style.display == 'none' ? '' : 'none');
     // }, 500);
-    new Promise((resolve, reject) => {
+    new Promise(function(resolve, reject) { //my code - I converted arrow function to vanilla JS
         f.style.outline = "#ffff00 solid 10px";
-        setTimeout(() => {
+        setTimeout(function(){ //my code - I converted arrow function to vanilla JS
             f.style.outline = "";
-            setTimeout(() => {
+            setTimeout(function() { //my code - I converted arrow function to vanilla JS
                 resolve();
             }, 250);
         }, 1000);
     });
 }
 
-//add playerSequence to convert player game clicks to numbers in sequence array;
-// function playerSequence() {
-//     let playerClick = $(this).attr("id");
-//     switch (playerClick) {
-//         case "topleft":
-//             playerOrder.push(1);//my code
-//             startGame(0);
-//             break;
-//         case "topright":
-//             playerOrder.push(2);//my code
-//             startGame(1);
-//             break;
-
-//         case "bottomleft":
-//             playerOrder.push(3);//my code
-//             startGame(2);
-//             break;
-
-//         case "bottomright":
-//             playerOrder.push(4);//my code
-//             startGame(3);
-//             break;
-//     }
-//     playerSequence();
-// }
-
-// function compareSequences() {
-//     if (playerSequence.indexOf(playerClick) === gameSequence().indexOf(n)) {
-//         nextSequence();
-//         score = +1;
-//         console.log("Excellent!");
-//     }
-// }
-
+//clear lit tile yellow outline
   function clearColor() {
     topLeft.style.outline = "#00ffff"; 
     topRight.style.outline = "#00ffff"; 
@@ -228,109 +168,103 @@ function blink(id) {
     bottomRight.style.outline = "#00ffff"; 
   }
   
-  function flashColor() {
+  //flash yellow oultine on game tiles
+  function flashYellow() {
     topLeft.style.backgroundColor = "#ffff00";
     topRight.style.backgroundColor = "#ffff00";
     bottomLeft.style.backgroundColor = "#ffff00";
     bottomRight.style.backgroundColor = "#ffff00";
   }
   
-  //now copy paste above for every corner
-  //free code camp
-  topLeft.addEventListener('click', (event) => {
-      playerOrder.push(1);//playerOrder is an array of the sections that the player has clicked
-      check();//a function to check if the player was right
-      playSound1();//my code - replaced free code camp's one()//this is the same function the computer called that lights up a color.
-      if(!win) { //if the player has not won yet, then we are going to set the timeout
-        setTimeout(() => {
-          clearColor(); //make sure the color the user clicked is going t oclear after 300 milliseconds
+//make game tiles clickable for the player
+  topLeft.addEventListener('click', function(event) { 
+      userPlay.push(1);
+      check();//a function to check if the player's moves are correct
+      playSound1();
+      if(!win) {
+        setTimeout(function() { 
+          clearColor(); //make sure the color the player clicked is going to clear after 300 milliseconds
         }, 300);
       }
   });
-  topRight.addEventListener('click', (event) => {
-      playerOrder.push(2);
+
+  topRight.addEventListener('click', function(event) { 
+      userPlay.push(2); 
       check();
-      playSound2();//my code - replaced free code camp's two(); //this is the same function the computer called that lights up a color.
+      playSound2();
       if(!win) {
-        setTimeout(() => {
+        setTimeout(function() {  
           clearColor();
         }, 300);
       }
   });
   
-  bottomLeft.addEventListener('click', (event) => {
-      playerOrder.push(3);
+  bottomLeft.addEventListener('click', function(event) { 
+      userPlay.push(3); 
       check();
-      playSound3();//my code - replaced free code camp's three(); //this is the same function the computer called that lights up a color.
+      playSound3(); 
       if(!win) {
-        setTimeout(() => {
+        setTimeout(function() { 
           clearColor();
         }, 300);
       }
   });
   
-  bottomRight.addEventListener('click', (event) => {
-      playerOrder.push(4);
+  bottomRight.addEventListener('click', function(event) { 
+      userPlay.push(4); 
       check();
-      playSound4();//my code - replaced free code camp's four(); //this is the same function the computer called that lights up a color.
+      playSound4();
       if(!win) {
-        setTimeout(() => {
+        setTimeout(function() {  
           clearColor();
         }, 300);
       }
   })
   
+// based on free Code Camp Simon Game tutorial 
 
-  //the above 4 corner functions all call the check() function which is the more compicated function
+//the above 4 functions call the complex check() function
 function check() {
-    if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
-      good = false;
-  
-    if (playerOrder.length == 20 && good) { //condition if player wins the game - you can change 3 to 20 to make the game harder so player wins after 20 rounds
-      winGame();
+    if (userPlay[userPlay.length - 1] !== gamePlay[userPlay.length - 1]) 
+      ace = false;
+  /*condition if player wins the game - you can change 3 to 20 to 
+  make the game harder so player wins after 20 rounds*/
+    if (userPlay.length == 3 && ace) { 
+      winGame(); 
     }
-  //condition if player is wrong
-    if (good == false) { //if the player got something wrong
-      flashColor();
-      turnCounter.innerHTML = "NO!";
-      setTimeout(() => {
-        turnCounter.innerHTML = turn;
+  //condition if player's moves are wrong
+    if (ace == false) { 
+      flashYellow();
+      roundCount.innerHTML = "YOU LOSE!";
+      setTimeout(function() { 
+        roundCount.innerHTML = round;
         clearColor();
-  
-        // if (strict) {
           play();//repeating the whole game starting the game over
-        // } else { //if we are not in strict mode then we can repeat the round instead of starting over.
-        //   compTurn = true;
-        //   flash = 0;
-        //   playerOrder = [];
-        //   good = true;
-        //   intervalId = setInterval(gameTurn, 800); //setting setIntrval just like the game function
-        // }
+
       }, 800);
-  
-      noise = false; //still in the good == false if statement meaning if the player got something wrong then we are not going to play a noise. we only play a noise if the player got the right thing. functions one, two, three, four check if noise=true
+      /*only run playSound functions when sound=true, on 
+      condition that the player's moves are correct */
+      sound = false; 
         
     }
-       //now there is the condition that the player got it wrong and has not won the game yet
-  
-    if (turn == playerOrder.length && good && !win) {
-      turn++;
-      playerOrder = [];
+    //condition if player's moves are right but has not won the game yet
+    if (round == userPlay.length && ace && !win) { 
+      round++;
+      userPlay = []; 
       compTurn = true;
       flash = 0;
-      turnCounter.innerHTML = turn;
+      roundCount.innerHTML = round;
       intervalId = setInterval(gameTurn, 800);
     }
   
   }
   
-  //if someone has won the game
+  //function if player has won the game
   function winGame() {
-    flashColor();
-    turnCounter.innerHTML = "WIN!";
-    on = false; //user cannot click anything because on = false
+    flashYellow();
+    roundCount.innerHTML = "CONGRATS! YOU WIN!";
+    //user cannot click any of the tiles while run = false
+    run = false; 
     win = true;
   }
   
-  
-  // JavaScript Document
